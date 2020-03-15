47fd6c20622ecc415206f4510174cd1dfe5b3975
FreakyCoder <kurayogun@gmail.com>
2020-03-15 19:11:31 +0300

    Static assets and static example data are not here anymore. Much better coding.

--- example/App.js
+++ example/App.js
@@ -1,6 +1,29 @@
 import React, { useState } from "react";
 import { StatusBar, SafeAreaView } from "react-native";
-import WhatsNew from "./lib/src/WhatsNewKit";
+import WhatsNew from "react-native-whatsnew-kit";
+
+const staticData = [
+  {
+    title: "Awesome",
+    description:
+      "Suspendisse ex mauris, viverra vitae mi eget, consectetur interdum sem. Phasellus sodales elit ac mauris posuere",
+    icon: require("./assets/check.png"),
+    iconComponent: null
+  },
+  {
+    title: "Dark Theme",
+    description:
+      "Orci varius. Aliquam accumsan lectus lorem, a congue diam commodo sit amet. Fusce laoreet sapien non lectus dignissim viverra.",
+    icon: require("./assets/theme.png"),
+    iconComponent: null
+  },
+  {
+    title: "Bug Fixes",
+    description: "Donec non mauris sagittis, gravida velit vel, varius quam.",
+    icon: require("./assets/wiping.png"),
+    iconComponent: null
+  }
+];
 
 const App = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -9,6 +32,7 @@ const App = () => {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <WhatsNew
+          data={staticData}
           fullScreen={false}
           isVisible={isVisible}
           onBackdropPress={() => setIsVisible(false)}
